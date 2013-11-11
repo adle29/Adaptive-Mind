8228671581569bad02762d0d2e5c43b341ab2be0
Abraham <aadberstein29@hotmail.com>
2013-11-11 00:28:29 -0600

    init58

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,1 +1,33 @@
-require.config({baseUrl:"/js",paths:{jQuery:"libs/jquery",jQueryAI:"libs/jqueryAI",bootJs:"libs/bootstrap.min",d3:"libs/d3",doc:"libs/doc",pep:"libs/pep",models:"models",Underscore:"libs/underscore",Backbone:"libs/backbone",text:"/js/libs/text",templates:"../templates",AdaptiveMindView:"AdaptiveMindView"},waitSeconds:200,shim:{jQueryAI:["jQuery"],d3:["jQuery"],pep:["jQuery"],bootJs:["jQuery"],doc:["jQuery"],Backbone:["Underscore","jQuery","doc","pep","jQueryAI","bootJs","d3"],AdaptiveMind:["Backbone"]}});require(["AdaptiveMind"],function(e){e.initialize()})
+require.config({
+  baseUrl: '/js', 
+  paths: { 
+    jQuery: 'libs/jquery',
+    jQueryAI: 'libs/jqueryAI', 
+    bootJs: 'libs/bootstrap.min',
+    d3: 'libs/d3',
+    doc: 'libs/doc',
+    pep: 'libs/pep',
+    models: 'models',
+    Underscore: 'libs/underscore',
+    Backbone: 'libs/backbone',
+    text: '/js/libs/text',
+    templates: '../templates',
+    AdaptiveMindView: 'AdaptiveMindView'
+  },
+  waitSeconds: 200,
+
+  shim: {
+    'jQueryAI': ['jQuery'],
+    'd3': ['jQuery'], 
+    'pep': ['jQuery'],
+    'bootJs': ['jQuery'], 
+    'doc': ['jQuery'],
+    'Backbone': ['Underscore', 'jQuery', 'doc', 'pep','jQueryAI', 'bootJs', 'd3'],
+    'AdaptiveMind': ['Backbone']
+  }
+});
+
+require(['AdaptiveMind'], function(AdaptiveMind) {
+  AdaptiveMind.initialize();
+});
+
--- public/js/views/index.js
+++ public/js/views/index.js
@@ -1,14 +1,18 @@
-
-define(['AdaptiveMindView', 'text!templates/index.html'],
-function(AdaptiveMindView, indexTemplate) {
-    var indexView = AdaptiveMindView.extend ({
-    	el: $('#content'),
-
-    	render: function() {
-      		this.$el.html(indexTemplate);
-    	}
-
-    }); 
+define([], function (){
+  function initPage(callback) {
+    require(['AdaptiveMindView', 'text!templates/index.html'], function(AdaptiveMindView, indexTemplate) {
     
-    return indexView;
-});
+      var indexView = AdaptiveMindView.extend ({
+        el: $('#content'),
+    
+        render: function() {
+          this.$el.html(indexTemplate);
+        }
+    
+      });//var
+    
+      callback( new indexView);
+    });
+  }//function
+  return {initPage: initPage};
+});//define
