7ffcaa1619810bce707fc715d94b86ad8d42de11
Abraham <aadberstein29@hotmail.com>
2013-11-16 15:34:54 -0600

    59

--- public/js/models/Account.js
+++ public/js/models/Account.js
@@ -1,4 +1,4 @@
-define(['models/vinBooksCollection', 'models/GroupCollection'], 
+define(['models/vinBooksCollection'], 
 	function(VinBooksCollection, GroupCollection) {
 
   var Account = Backbone.Model.extend({
@@ -7,8 +7,6 @@ define(['models/vinBooksCollection', 'models/GroupCollection'],
     initialize: function() {
        this.vinbooks       = new VinBooksCollection();
        this.vinbooks.url   = '/accounts/' + this.id + '/vinbook';
-       this.groups 		   = new GroupCollection();
-       this.groups.url     = '/accounts/' + this.id + '/group';
        this.me = this.id;
        this.fetch();
        this.bind('change', this.render, this);
